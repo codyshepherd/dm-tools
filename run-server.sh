bfe4113f31ed625d92a24e54a79010598aed69fb
Cody Shepherd <cody.shepherd@gmail.com>
2021-08-09 16:12:30 -0700

    Add script to run API server

---
+++ run-server.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -ex
+
+venv_name="dm-tools-server"
+
+/usr/bin/virtualenv -p $(which python3) $venv_name 
+source $venv_name/bin/activate
+
+pip install .
+
+cd api-server
+
+uvicorn --workers 2 --access-log --use-colors --limit-concurrency 10 --backlog 10 --host 0.0.0.0 --port 443 --ssl-keyfile /etc/letsencrypt/live/api.dm-tools.dev/privkey.pem --ssl-certfile /etc/letsencrypt/live/api.dm-tools.dev/fullchain.pem  main:app
